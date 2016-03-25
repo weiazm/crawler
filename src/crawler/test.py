576b7ba110cb611e354b9553b305d79e60e6a21a
weihongyan <weiazm@163.com>
2016-03-25 22:49:23 +0800

    “测试数据库插入”

--- src/crawler/test.py
+++ src/crawler/test.py
@@ -4,4 +4,10 @@ Created on 2016年3月25日
 
 @author: why
 '''
+import MySQLdb
+db = MySQLdb.connect("localhost","root","1234","crawler" )
+cursor = db.cursor()
+cursor.execute("insert into test value(3,\"safaf\",1)")
+db.commit()
+db.close()
 print "fuckyou"
