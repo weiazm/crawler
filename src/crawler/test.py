64551f91e518a327049972b5df8a5bdbc795dab7
weihongyan <weiazm@163.com>
2016-03-29 07:55:19 +0800

    廖雪峰教程桥代码

--- src/crawler/test.py
+++ src/crawler/test.py
@@ -1,11 +1,13 @@
-###-*-encoding:utf-8-*-
+# ##-*-encoding:utf-8-*-
 '''
 Created on 2016年3月25日
 
 @author: why
 '''
 import MySQLdb
-db = MySQLdb.connect("localhost","root","1234","crawler" )
+
+
+db = MySQLdb.connect("localhost", "root", "1234", "crawler")
 cursor = db.cursor()
 cursor.execute("insert into test value(3,\"safaf\",1)")
 db.commit()
