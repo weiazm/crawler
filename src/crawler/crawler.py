549db7871dbb39fad55ab6b32cdd339f167a26ba
weihongyan <weiazm@163.com>
2016-04-05 14:12:09 +0000

    一些测试代码

---
+++ src/crawler/crawler.py
@@ -0,0 +1,9 @@
+#-*-coding:utf-8-*-
+'''
+Created on 2016年4月5日
+
+@author: why
+'''
+
+if __name__ == '__main__':
+    pass
--- src/pakage/test.py
+++ src/pakage/test.py
@@ -5,11 +5,27 @@ Created on 2016年3月25日
 @author: why
 '''
 import MySQLdb
+from pyodbc import Row
 
 
-db = MySQLdb.connect("localhost", "root", "1234", "pakage")
-cursor = db.cursor()
+conn = MySQLdb.connect("localhost", "root", "1234", "crawler")
+cursor = conn.cursor()
 cursor.execute("insert into test value(3,\"safaf\",1)")
-db.commit()
-db.close()
-print "fuckyou"
+conn.commit()
+conn.close()
+
+conn = MySQLdb.connect("localhost", "root", "1234", "crawler")
+cursor = conn.cursor()
+cursor.execute("select * from test")
+for row in cursor.fetchall():
+    print row
+print cursor.fetchall
+conn.commit()
+conn.close()
+
+conn = MySQLdb.connect("localhost", "root", "1234", "crawler")
+cursor = conn.cursor()
+n=cursor.execute("delete from test where id=%s","3")
+print n
+conn.commit()
+conn.close()
