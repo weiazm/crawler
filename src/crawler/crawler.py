f93d23111d6179c24acec88aae452be8812eddbf
weihongyan <weiazm@163.com>
2016-04-06 03:24:40 +0000

    some changes

--- src/crawler/crawler.py
+++ src/crawler/crawler.py
@@ -4,6 +4,19 @@ Created on 2016年4月5日
 
 @author: why
 '''
+import urllib2
+from bs4 import BeautifulSoup
 
-if __name__ == '__main__':
-    pass
+def gethtml(url):
+    req = urllib2.Request(url,headers={'User-Agent':'Magic Browser'})
+    usock=urllib2.urlopen(req)
+    encoding = usock.headers.getparam('charset')
+    print encoding
+    html = usock.read()
+    usock.close()
+    return html
+
+html=gethtml("http://club.autohome.com.cn/bbs/forum-c-3204-1.html")
+print html
+soup = BeautifulSoup(html,"lxml")
+#print soup.get_text()
