dcbf987c5b47e819d54fd4dfed68594c7c5a710a
weihongyan <weiazm@163.com>
2016-04-17 19:30:05 +0800

    副本4

--- src/refactor crawler/RunnableGetContent4.py
+++ src/refactor crawler/RunnableGetContent4.py
@@ -11,14 +11,14 @@ from SoupUtil import SoupOperator
 from Constant import BBSContent
 
 conn = mysql.connector.connect(user='root', password='1234', database='refactor_crawler', use_unicode=True)
-logging.basicConfig(filename='log3.log', level=logging.DEBUG)
+logging.basicConfig(filename='log4.log', level=logging.DEBUG)
 
 cur = conn.cursor()
-cur.execute('SELECT num FROM counter where id = 3')
+cur.execute('SELECT num FROM counter where id = 4')
 index = cur.fetchall()[0][0]
 #for x in range(1,1766627):
 
-while index<1499999:
+while index<1766626:
     x=index+1
     mo = MysqlOperator(conn)
     bbsContent = BBSContent()
@@ -68,6 +68,6 @@ while index<1499999:
         continue
     else:
         index+=1
-        cur.execute('update counter set num = %s where id = 3',[index])
+        cur.execute('update counter set num = %s where id = 4',[index])
         conn.commit()
 
